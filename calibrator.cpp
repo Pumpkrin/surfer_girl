d3554b37ab110bcd6642b7972454d02bffc5f507
Alexandre Sécher <asecher@iphc.cnrs.fr>
2021-06-07 12:46:13 +0200

    removed fit if source_file_c < 2

--- calibrator.cpp
+++ calibrator.cpp
@@ -97,7 +97,7 @@ int main( int argc, char* argv[]) {
     }
        
 //    TF1 f{"f", "[0]+x*[1]", 0, energy_c.back() * 1.2};
-    if( !source_file_c.empty() ) { 
+    if( source_file_c.size() > 2 ) { 
         TF1 f{"f", "[0]+x*[1]", 0, 24 };
         g_h->Fit(&f, "R");
     }
